b0625350664daf1c330cd4d1e3235a6d01695e7f
leo <leolson.orion@gmail.com>
2018-08-09 14:37:31 +0200

    Initial commit

---
+++ JavaJoke/src/main/java/com/example/javajoke/JavaJoke.java
@@ -0,0 +1,9 @@
+package com.example.javajoke;
+
+public class JavaJoke {
+
+    public String jokeRequest(){
+
+        return "Why did the chicken cross the road? To get to the other side";
+    }
+}
